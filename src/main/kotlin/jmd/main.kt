b5ab7ac9f6545bde29545327cbd4bc49b42f0740
msrocka <michael.srocka@gmail.com>
2018-12-17 16:57:10 +0100

    initial commit

---
+++ src/main/kotlin/jmd/main.kt
@@ -0,0 +1,30 @@
+package jmd
+
+import com.github.javaparser.JavaParser
+import com.github.javaparser.ast.body.MethodDeclaration
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter
+import java.io.File
+
+class Handler : VoidVisitorAdapter<Void>() {
+
+    override fun visit(m: MethodDeclaration, arg: Void?) {
+        println("### ${m.name}")
+        println(m.comment)
+    }
+
+}
+
+
+fun main(args: Array<String>) {
+    val path =
+        "C:\\Users\\Besitzer\\Projects\\git_repos\\olca-modules\\olca-core\\src\\main\\java\\org\\openlca\\core\\results"
+    val dir = File(path)
+    val model = Model()
+    dir.listFiles().forEach { file ->
+        if (file.name.endsWith(".java")) {
+            val cu = JavaParser.parse(file)
+            model.addCU(cu)
+        }
+    }
+    println(model.markdown())
+}
---
+++ src/main/kotlin/jmd/model.kt
@@ -0,0 +1,151 @@
+package jmd
+
+import com.github.javaparser.ast.CompilationUnit
+import com.github.javaparser.ast.body.FieldDeclaration
+import com.github.javaparser.ast.body.TypeDeclaration
+import com.github.javaparser.ast.comments.Comment
+import java.util.*
+
+
+fun formatComment(comment: Optional<Comment>): String {
+    if (!comment.isPresent)
+        return "_undocumented_\n"
+    val c = comment.get()
+    if (!c.isJavadocComment)
+        return "_undocumented_\n"
+    var s = ""
+    c.content.lines().forEach { line ->
+        var l = line.trim()
+        if (l.startsWith("/**")) {
+            l = l.substring(3)
+        }
+        if (l.endsWith("*/")) {
+            l = l.substring(0, l.length - 2)
+        }
+        if (l.startsWith("* ")) {
+            l = l.substring(2)
+        }
+        l = l.trim()
+        if (!l.isEmpty()) {
+            if (l == "*") {
+                l = ""
+            }
+            s += "$l\n"
+        }
+    }
+    return s
+}
+
+class Model {
+
+    val packages = mutableMapOf<String, Package>()
+
+    fun getPackage(name: String): Package {
+        var p = packages[name]
+        if (p != null)
+            return p
+        p = Package(name)
+        packages[name] = p
+        return p
+    }
+
+    fun addCU(cu: CompilationUnit) {
+        val pd = cu.packageDeclaration
+        val pack = if (pd.isPresent) {
+            getPackage(pd.get().nameAsString)
+        } else {
+            getPackage("default")
+        }
+        cu.types.forEach { t ->
+            if (t.isPublic) {
+                pack.addType(t)
+            }
+        }
+    }
+
+    fun markdown(): String {
+        var s = ""
+        val packs = packages.values.sortedBy { p ->
+            p.name
+        }
+        packs.forEach { p ->
+            s += p.markdown()
+        }
+        return s
+    }
+
+}
+
+class Package(val name: String) {
+
+    val types = mutableListOf<Type>()
+
+    fun addType(t: TypeDeclaration<*>) {
+        types.add(Type(name + "." + t.name, t))
+    }
+
+    fun markdown(): String {
+        var s = "# Package: $name\n\n"
+        types.sortBy { it.name }
+        types.forEach {
+            s += it.markdown()
+        }
+        return s
+    }
+}
+
+class Type(val id: String, t: TypeDeclaration<*>) {
+
+    val name = t.nameAsString
+    val doc = formatComment(t.comment)
+    val fields = mutableListOf<Field>()
+    var superTypes: String? = null
+
+    init {
+        t.fields.forEach { f ->
+            if (f.isPublic) {
+                fields.add(Field(f))
+            }
+        }
+        if (t.isClassOrInterfaceDeclaration) {
+            val decl = t.asClassOrInterfaceDeclaration()
+            var extends = ""
+            decl.extendedTypes.forEach { s ->
+                if (extends != "") {
+                    extends += ", "
+                }
+                extends += s.nameAsString
+            }
+            if (extends != "") {
+                superTypes = extends
+            }
+        }
+    }
+
+    fun markdown(): String {
+        // var s = "<a name=\"$id\"></a>\n\n"
+        var s = "## $name"
+        if (superTypes != null) {
+            s += " < $superTypes"
+        }
+        s += "\n\n$doc\n\n"
+        fields.sortBy { it.name }
+        fields.forEach {
+            s += it.markdown()
+        }
+        return s
+    }
+}
+
+class Field(d: FieldDeclaration) {
+
+    val name = d.variables[0].nameAsString
+    val doc = formatComment(d.comment)
+    val type = d.variables[0].typeAsString
+
+    fun markdown(): String {
+        var s = "### $name : $type\n\n"
+        s += "$doc\n\n"
+        return s
+    }
+}
